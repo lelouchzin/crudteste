04a15c3f20832fcf7f455773b77847ccfb5f2dbc
lelouch <91700449+lelouchf@users.noreply.github.com>
2025-04-29 15:02:09 -0300

    CRUD COMPLETO

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,18 +1,49 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { RecadosService } from './recados.service';
 
 @Controller('recados')
 export class RecadosController {
   constructor(private readonly recadosservice: RecadosService) {}
 
+  // @HttpStatus(HttpStatus.OK)
   @Get()
-  findAll() {
-    return this.recadosservice.getFindAll();
+  findAll(@Query() paginacao: { limit: number; offset: number }) {
+    paginacao.limit = 10;
+    paginacao.offset = 0;
+    return this.recadosservice.getFindAll(paginacao);
   }
 
+  // @HttpStatus(HttpStatus.OK)
   @Get(':id')
   findOne(@Param('id') id: string) {
     console.log(id);
     return this.recadosservice.getFindOne(id);
+  }
+
+  // @HttpStatus(HttpStatus.CREATED)
+  @Post()
+  create(@Body() body: string) {
+    console.log(body);
+    return this.recadosservice.create(body);
+  }
+
+  // @HttpStatus(HttpStatus.OK)
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: string) {
+    return this.recadosservice.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.recadosservice.remove(id);
   }
 }
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -2,11 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RecadosService {
-  getFindAll() {
-    return 'This action returns all recados';
+  getFindAll(paginacao: { limit: number; offset: number }) {
+    return `This action returns all recados, limit=#${paginacao.limit}, offset=#${paginacao.offset}`;
   }
 
   getFindOne(id: string) {
     return `This action returns a #${id} recado`;
+  }
+
+  create(body: string) {
+    body = 'This action create a recado';
+    return body;
+  }
+
+  update(id: string, body: string) {
+    body = `This action updates a #${id} recado`;
+    return body;
+  }
+
+  remove(id: any) {
+    return `This action removes a #${id} recado`;
   }
 }
